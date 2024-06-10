93722776d40744a05d83855d290d5ee6d23a29bb
Rob Long <roblong@gmail.com>
2024-06-09 19:12:07 -0700

    release: Add release workflow, semantic-release setup, and plugins

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,9 +1,27 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore']],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'ci',
+        'revert',
+        'build',
+        'release',
+        'deps',
+      ],
+    ],
     'subject-case': [2, 'always', ['sentence-case']],
     'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 72]
-  }
+    'header-max-length': [2, 'always', 72],
+  },
 };
